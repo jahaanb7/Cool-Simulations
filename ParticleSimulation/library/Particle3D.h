7283dea480bcd15683c1400c1e907c26392ae02c
Jahaan <jeyubhavsar@gmail.com>
2026-02-21 18:42:30 -0500

    added comments and cleaned up code

---
+++ ParticleSimulation/library/Particle3D.h
@@ -0,0 +1,128 @@
+#include <glm/glm.hpp>
+#include <cmath>
+
+class Particle3D{
+  public:
+    glm::vec3 position, velocity;
+    glm::vec3 acceleration;
+
+    // additional vars
+    float mass, radius;
+    float damping = 0.96f;
+
+    Particle3D(glm::vec3 pos, glm::vec3 vel, float m, float r){
+      position = pos; 
+      velocity = vel;
+      mass = m;
+      radius = r;
+
+      acceleration = glm::vec3(0.0f, -98.0f, 0.0f);
+    }
+
+  void drawParticle3D(int lats, int longs){
+    for(int i = 0; i < lats; i++){
+      float lat0 = M_PI * (-0.5f + (float)i / lats);
+      float z0  = sin(lat0) * radius;
+      float zr0 = cos(lat0) * radius;
+
+      float lat1 = M_PI * (-0.5f + (float)(i+1) / lats);
+      float z1 = sin(lat1) * radius;
+      float zr1 = cos(lat1) * radius;
+
+      glBegin(GL_TRIANGLE_STRIP);
+      for(int j = 0; j <= longs; j++){
+
+        float lng = 2 * M_PI * (float)(j) / longs;
+        float x = cos(lng);
+        float y = sin(lng);
+
+        glm::vec3 v1 = position + glm::vec3(x*zr0, y*zr0, z0);
+        glm::vec3 n1 = glm::normalize(glm::vec3(x*zr0, y*zr0, z0));
+
+        glNormal3f(n1.x, n1.y, n1.z);
+        glVertex3f(v1.x, v1.y, v1.z);
+
+        glm::vec3 v2 = position + glm::vec3(x*zr1, y*zr1, z1);
+        glm::vec3 n2 = glm::normalize(glm::vec3(x*zr1, y*zr1, z1));
+
+        glNormal3f(n2.x, n2.y, n2.z);
+        glVertex3f(v2.x, v2.y, v2.z);
+      }
+      glEnd();
+    }
+  }
+
+  void updatePosition3D(float deltaTime){
+      velocity += acceleration * deltaTime;
+      position += velocity * deltaTime;
+  }
+
+  void boundingBoundary3D(int WIDTH, int HEIGHT){
+    float boundsX = WIDTH/2.0f;
+    float boundsY = HEIGHT/2.0f;
+    float boundsZ = 400.0f;
+
+    if(position.x + radius > boundsX){
+        position.x = boundsX - radius;
+        velocity.x *= -1.0f;
+    }
+
+    if(position.x - radius < -boundsX){
+        position.x = -boundsX + radius;
+        velocity.x *= -1.0f;
+    }
+
+    if(position.y + radius > boundsY){
+        position.y = boundsY - radius;
+        velocity.y *= -1.0f;
+    }
+
+    if(position.y - radius < -boundsY){
+        position.y = -boundsY + radius;
+        velocity.y *= -1.0f;
+    }
+
+    if(position.z + radius > boundsZ){
+        position.z = boundsZ - radius;
+        velocity.z *= -1.0f;
+    }
+
+    if(position.z - radius < -boundsZ){
+        position.z = -boundsZ + radius;
+        velocity.z *= -1.0f;
+    }
+  }
+
+  void checkSphereCollision(int boundaryRadius)
+  {
+    float distance = glm::length(position);
+
+    if(distance + radius >= boundaryRadius)
+    {
+        glm::vec3 normal = glm::normalize(position);
+
+        velocity = velocity - 2.0f * glm::dot(velocity, normal) * normal;
+        velocity *= damping;
+        position = normal * (boundaryRadius - radius);
+    }
+  }
+
+  void Particle3DCollision(Particle3D& compareParticle){
+    glm::vec3 delta = compareParticle.position - position;
+    float distance = glm::length(delta);
+    float minDistance = radius + compareParticle.radius;
+
+    if(distance < minDistance){
+      
+      glm::vec3 temp = velocity;
+      velocity = compareParticle.velocity * damping;
+      compareParticle.velocity = temp * damping;
+
+      glm::vec3 normal = glm::normalize(delta);
+      float overlap = minDistance - distance;
+
+      position -= normal * (overlap * 0.5f);
+      compareParticle.position += normal * (overlap * 0.5f);
+    }
+  }
+};
---
+++ ParticleSimulation/main.cpp
@@ -0,0 +1,243 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp> 
+#include <glm/gtc/constants.hpp>
+#include "library/Particle.h"
+#include "library/Particle3D.h"
+#include <vector>
+#include <cmath>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <cstdlib>
+
+/*
+Particle Simulation:
+
+
+*/
+
+// Screen Dimension variables
+const float WIDTH = 800.0f;
+const float HEIGHT = 600.0f;
+
+// Frame Timing, and delay in particle drawing
+float elapsedTime = 0.0f;
+float deltaTime = 0.0167;
+float TimeDelay = 0.5f;
+
+// Array of Particles and spawnTime (float)
+std::vector<Particle3D> particles;
+std::vector<float> spawnTimes;
+
+void WindowResize(GLFWwindow* window, int width, int height){
+    glViewport(0, 0, width, height);
+}
+
+// 2D Boundary Box - collision and containment
+void drawBoundaryBox(float width, float height, float depth) {
+    float x = width / 2.0f;
+    float y = height / 2.0f;
+    float z = depth; 
+
+    glDisable(GL_LIGHTING);
+    glColor3f(1.0f,1.0f,1.0f); 
+
+    glBegin(GL_LINES);
+
+    glVertex3f(-x, -y, -z); glVertex3f( x, -y, -z);
+    glVertex3f( x, -y, -z); glVertex3f( x,  y, -z);
+    glVertex3f( x,  y, -z); glVertex3f(-x,  y, -z);
+    glVertex3f(-x,  y, -z); glVertex3f(-x, -y, -z);
+
+    glVertex3f(-x, -y,  z); glVertex3f( x, -y,  z);
+    glVertex3f( x, -y,  z); glVertex3f( x,  y,  z);
+    glVertex3f( x,  y,  z); glVertex3f(-x,  y,  z);
+    glVertex3f(-x,  y,  z); glVertex3f(-x, -y,  z);
+
+    glVertex3f(-x, -y, -z); glVertex3f(-x, -y,  z);
+    glVertex3f( x, -y, -z); glVertex3f( x, -y,  z);
+    glVertex3f( x,  y, -z); glVertex3f( x,  y,  z);
+    glVertex3f(-x,  y, -z); glVertex3f(-x,  y,  z);
+
+    glEnd();
+
+    glEnable(GL_LIGHTING);
+}
+
+// 3D Boundary Sphere - Visualization
+void drawBoundarySphere(int lats, int longs, int radius){
+
+    // Disabled Lighting so that it doesnt effect the boundary visualization
+    glDisable(GL_LIGHTING);
+
+    glColor3f(1.0f,1.0f,1.0f); 
+
+    // Uses Legacy Opengl for creating singular points (GL_POINTS)
+    glBegin(GL_POINTS);
+    for(int i = 0; i <= lats; i++){
+
+        float theta = i * M_PI/lats;
+        for(int j = 0; j <= longs; j++){
+
+            float phi = j * 2 * M_PI/longs;
+
+            float x = radius * sin(theta) * cos(phi);
+            float y = radius * sin(theta) * sin(phi);
+            float z = radius * cos(theta);
+
+            glVertex3f(x, y, z);
+        }
+    }
+    glEnd();
+    glEnable(GL_LIGHTING);
+}
+
+void drawParticleArray2D(std::vector<Particle>& particles, float deltaTime){
+
+    // auto& just gets the type of array it is automatically and is referenced
+    for(auto& p : particles){        
+        p.updatePosition(deltaTime);
+        p.boundingBoundary(WIDTH, HEIGHT);
+        p.drawParticle();
+    }
+}
+
+void drawParticleArray3D(std::vector<Particle3D>& particles, float deltaTime){
+    for(int i = 0; i < particles.size(); i++){
+        // elapsedTime is for the time delay in drawing each particle
+        if(elapsedTime >= spawnTimes[i]){
+            particles[i].updatePosition3D(deltaTime);
+
+            // Detect collision for iteration i against iteration i + 1 (j)
+            for(int j = i+1; j < particles.size(); j++){
+                particles[i].Particle3DCollision(particles[j]);
+            }
+
+            // Boundary Sphere collision
+            particles[i].checkSphereCollision(400);
+
+            particles[i].drawParticle3D(10,10);
+        }
+    }
+}
+
+int main(void)
+{
+    // initialize glfw
+    if (!glfwInit()){
+        return -1;
+    }
+
+    // create GLFWwindow
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Space Simulation", NULL, NULL);
+
+    // terminate glfw if the window is not found
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    // connect it to openGl (context)
+    glfwMakeContextCurrent(window);
+
+    // initializes all OpenGL function pointers (retrieves openGL functions)
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // resize window
+    glfwSetFramebufferSizeCallback(window, WindowResize);
+
+    // Depth Test (DepthBuffer)
+    glEnable(GL_DEPTH_TEST);
+
+    // Enable positional lighting that
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+
+    //initialize Particle
+    glm::vec3 position(0.0f, 200.0f, 0.0f);
+
+    // spherical rotation and direction
+    const float magnitude = 400.0f;
+    const float anglePhi = glm::radians(-90.0f);
+    const float angleTheta = glm::radians(1.0f);
+
+    // for rotational and directional control
+    float directionX = magnitude*(cos(anglePhi)*cos(angleTheta));
+    float directionY = magnitude*(sin(anglePhi)*sin(angleTheta));
+    float directionZ = magnitude*(sin(angleTheta));
+
+    // set a positional value to velocity
+    float velocityX = 0.0f;
+    float velocityY = -200.0f;
+    float velocityZ = 0.0f;
+
+    glm::vec3 velocity = glm::vec3(directionX, directionY, directionZ);
+
+    // variables for particle initialization
+    const float mass = 300.0f;
+    const float radius = 10.0f;
+    int numParticles = 100;
+
+    // append numParticles to the array of particles and to spawnTimes array
+    for(int i = 0; i < numParticles; i++){
+        particles.emplace_back(position, velocity, mass, radius);
+        spawnTimes.push_back(i * TimeDelay); // each particle spawns after 0.1s
+    }
+
+    // Testing particle
+    Particle3D TestParticle = Particle3D(position, velocity, mass, 30.0f);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Perspective Projection (Perspective Matrix)
+        glm::mat4 projection = glm::perspective(glm::radians(60.0f), WIDTH / HEIGHT, 0.1f, 2000.0f);
+
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glLoadMatrixf(glm::value_ptr(projection));
+
+        // View pipeline (Camera, ViewModel Matrix)
+        glm::vec3 cameraPos(800.0f , 300.0f, 600.0f);
+        glm::vec3 cameraTarget(0.0f,0.0f,0.0f);
+        glm::vec3 cameraUp(0.0f,1.0f,0.0f);
+
+        glm::mat4 view = glm::lookAt(cameraPos, cameraTarget, cameraUp);
+
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glLoadMatrixf(glm::value_ptr(view));
+
+        // set light as positional and set (x,y,z) coordinates
+        GLfloat light[] = {400.0f, 300.0f, 200.0f, 1.0f};
+        glLightfv(GL_LIGHT0, GL_POSITION, light);
+
+        // Update
+        elapsedTime += deltaTime;
+        drawParticleArray3D(particles, deltaTime);
+
+        /* 
+        TestParticle.drawParticle3D(10,10);
+        TestParticle.updatePosition3D(deltaTime);
+        TestParticle.checkSphereCollision(400);
+        
+        for(int i = 0; i < particles.size(); i++){
+            TestParticle.Particle3DCollision(particles[i]);
+        }
+        */
+
+        drawBoundarySphere(20, 20, 400);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
